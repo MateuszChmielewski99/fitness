dba1b691fc33dd71747ec1dcc518c01f309cb547
Mateusz Chmielewski <chmielewski079@gmail.com>
2023-05-14 15:58:36 +0200

    ci: added deploy pipeline

--- .github/scripts/dispatch.ps1
+++ .github/scripts/dispatch.ps1
@@ -2,11 +2,10 @@ param (
     [string] $Base,
     [string] $Head
 )
-Write-Host "base: $Base"
-Write-Host "head: $Head"
 
 $nxAffected = pnpm nx print-affected --base=$Base --head=$Head --select=projects --plain
 $projects = $nxAffected -split ","
 foreach ($project in $projects) {
     Write-Host "Affected project: $project"
+    gh workflow run deploy.yaml -f name=$project
 }
