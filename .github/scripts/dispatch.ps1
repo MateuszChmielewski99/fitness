e68ad4ec3fc1a35f4b32f6efbee7c5157eb90a5f
Mateusz Chmielewski <chmielewski079@gmail.com>
2023-05-14 15:23:38 +0200

    ci: 😒

--- .github/scripts/dispatch.ps1
+++ .github/scripts/dispatch.ps1
@@ -1,5 +1,6 @@
+param ($base)
 Write-Output "Hejo"
-$nxAffected = pnpm nx print-affected --base=origin/main  --select=projects --plain
+$nxAffected = pnpm nx print-affected --base=$base --select=projects --plain
 $projects = $nxAffected -split ","
 foreach ($project in $projects) {
     Write-Host "Affected project: $project"
