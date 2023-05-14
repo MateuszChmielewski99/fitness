11f06f27be67ba9b21331508bef5929e138b2eaf
Mateusz Chmielewski <chmielewski079@gmail.com>
2023-05-14 18:04:31 +0200

    fix: build

--- .github/scripts/dispatch.ps1
+++ .github/scripts/dispatch.ps1
@@ -5,7 +5,7 @@ param (
 
 gh auth login
 
-$nxAffected = pnpm nx print-affected --base=$Base --head=$Head --select=projects --plain
+$nxAffected = pnpm nx print-affected --base=origin/main --select=projects --plain
 $projects = $nxAffected -split ","
 foreach ($project in $projects) {
     Write-Host "Affected project: $project"
--- apps/server/src/routers/test.ts
+++ apps/server/src/routers/test.ts
@@ -2,5 +2,5 @@ import { testSchema } from "../models/input";
 import { t } from "../trpc";
 
 export const test = t.router({
-    hello: t.procedure.input(testSchema).query(async req => {return {a:12}})
+    hello: t.procedure.input(testSchema).query(() => {return {a:12}})
 })
