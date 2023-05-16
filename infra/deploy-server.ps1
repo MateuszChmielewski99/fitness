e09d5bc62138abda1c7c4c7620ab610cee347935
Mateusz Chmielewski <chmielewski079@gmail.com>
2023-05-16 20:45:17 +0200

    hotfix

--- infra/deploy-server.ps1
+++ infra/deploy-server.ps1
@@ -1,5 +1,5 @@
 $scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Path
-$appPath = Join-Path $scriptPath "..\apps\server"
+$appPath = Join-Path $scriptPath "..\apps\server\dist"
 Set-Location $appPath
 
 Write-Host "Deploying server"
